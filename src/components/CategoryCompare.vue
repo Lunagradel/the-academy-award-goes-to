f0d52abe52b9f4e5cfc6c2549192f01fa3c6c917
Luna Gradel <lunagradel@gmail.com>
2020-02-06 00:43:29 +0100

    compare wip.

---
+++ src/components/CategoryCompare.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="category">
+    <h3 class="category-title">{{ category.title }}</h3>
+    <div class="nominees">
+        <div class="nominee" v-for="nominee in category.nominees" :key="nominee.id">
+
+            <div class="nominee-compare-info">
+                <div></div>
+                <div></div>
+            </div>
+            <div class="nominee-info">
+                <p class="nominee-main" v-if="category.isFilmNominated">{{nominee.film}}</p>
+                <p class="nominee-main" v-else v-for="(person, idx) in nominee.persons" :key="idx" >{{person}}</p>
+
+                <p class="nominee-extra" v-if="!category.isFilmNominated">{{nominee.film}}</p>
+                <p class="nominee-extra" v-else v-for="(person, idx) in nominee.persons" :key="idx" >{{person}}{{idx !== nominee.persons.length - 1 ? ',' : '' }}</p>
+            </div>
+            
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'CategoryCompare',
+  props: {
+    category: Object,
+    voteOne: Object,
+    voteTwo: Object
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -27,6 +27,14 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/User.vue')
   },
   {
+    path: '/compare/:userOneId/:userTwoId',
+    name: 'compare',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Compare.vue')
+  },
+  {
     path: '/winners',
     name: 'winners',
     // route level code-splitting
