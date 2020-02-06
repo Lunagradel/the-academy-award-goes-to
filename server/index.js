a08da65a65f582f5ac3c2658c84f0b2d4dc755b8
Luna Gradel <lunagradel@gmail.com>
2020-02-06 18:32:13 +0100

    .

--- server/index.js
+++ server/index.js
@@ -15,7 +15,10 @@ const schema = mergeTypes(
 const apolloOptions = {
   typeDefs: gql(schema),
   resolvers,
-  cors: true
+  cors: {
+		origin: '*',			// <- allow request from all domains
+    credentials: true
+  }
 };
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useFindAndModify: false })
