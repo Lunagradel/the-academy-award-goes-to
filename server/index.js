81e6ead43e3d003a7256e0d9aed37a1ac6988e58
Luna Gradel <lunagradel@gmail.com>
2020-02-06 20:41:35 +0100

    .

--- server/index.js
+++ server/index.js
@@ -4,12 +4,9 @@ import "@babel/polyfill";
 import mongoose from 'mongoose';
 import { resolve } from 'path';
 import { mergeTypes, fileLoader } from 'merge-graphql-schemas';
-import { ApolloServer, gql } from 'apollo-server-express';
-import cors from 'cors';
+import { ApolloServer, gql } from 'apollo-server';
+
 import resolvers from './resolvers';
-
-const express = require('express');
-
 
 const schema = mergeTypes(
   fileLoader(resolve(__dirname, './schema'))
@@ -25,21 +22,11 @@ const apolloOptions = {
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useFindAndModify: false })
   .then(() => {
-    const apolloServer = new ApolloServer(apolloOptions)
-    const app = express();
-
-    apolloServer.applyMiddleware({app, cors: {
-      origin: '*',			// <- allow request from all domains
-      credentials: true
-    }});
-
-    app.use(cors());
-    
-    app.listen(
+    const apolloServer = new ApolloServer(apolloOptions).listen(
       {port: process.env.PORT || parseInt(process.env.SERVER_PORT)}
     )
 
-    return app;
+    return apolloServer;
   })
   .then(({ url, subscriptionsUrl }) => {
     console.log(`🚀 Server ready at ${url}`);
