16652c8fb0202ad0ef580c229150b1903f243ee5
Luna Gradel <lunagradel@gmail.com>
2020-02-06 21:05:18 +0100

    .

--- server/index.js
+++ server/index.js
@@ -4,14 +4,15 @@ import "@babel/polyfill";
 import mongoose from 'mongoose';
 import { resolve } from 'path';
 import { mergeTypes, fileLoader } from 'merge-graphql-schemas';
-import { ApolloServer, gql } from 'apollo-server';
+import { ApolloServer, gql } from 'apollo-server-express';
+import cors from 'cors';
+import express from 'express';
 
 import resolvers from './resolvers';
 
 const schema = mergeTypes(
   fileLoader(resolve(__dirname, './schema'))
 );
-
 
 const apolloOptions = {
   typeDefs: gql(schema),
@@ -20,15 +21,26 @@ const apolloOptions = {
   playground: true
 };
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useFindAndModify: false })
-  .then(() => {
-    const apolloServer = new ApolloServer(apolloOptions).listen(
-      {port: process.env.PORT || parseInt(process.env.SERVER_PORT)}
-    )
+const app = express();
+app.use(cors());
 
-    return apolloServer;
-  })
-  .then(({ url, subscriptionsUrl }) => {
-    console.log(`🚀 Server ready at ${url}`);
-    console.log(`🚀 Subscriptions ready at ${subscriptionsUrl}`);
-  });
+const apolloServer = new ApolloServer(apolloOptions);
+
+apolloServer.applyMiddleware({app, path: '/'});
+
+app.listen(process.env.PORT || parseInt(process.env.SERVER_PORT), () => {
+  mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useFindAndModify: false });
+});
+
+// mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useFindAndModify: false })
+//   .then(() => {
+//     const apolloServer = new ApolloServer(apolloOptions).listen(
+//       {port: process.env.PORT || parseInt(process.env.SERVER_PORT)}
+//     )
+
+//     return apolloServer;
+//   })
+//   .then(({ url, subscriptionsUrl }) => {
+//     console.log(`🚀 Server ready at ${url}`);
+//     console.log(`🚀 Subscriptions ready at ${subscriptionsUrl}`);
+//   });
