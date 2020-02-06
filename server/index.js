3d154deefbc9b195ad3745137272c0d80781aed4
Luna Gradel <lunagradel@gmail.com>
2020-02-06 23:41:05 +0100

    .

--- server/index.js
+++ server/index.js
@@ -20,14 +20,7 @@ const apolloOptions = {
 };
 
 const app = express();
-app.use(cors());
-
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
-  next();
-});
+app.use(cors({origin: '*'}));
 
 const apolloServer = new ApolloServer(apolloOptions);
 
