b455d2a8f8758549cf323e945c49bb68595e8f25
Luna Gradel <lunagradel@gmail.com>
2020-02-06 21:10:23 +0100

    .

--- server/index.js
+++ server/index.js
@@ -16,9 +16,7 @@ const schema = mergeTypes(
 
 const apolloOptions = {
   typeDefs: gql(schema),
-  resolvers,
-  introspection: true,
-  playground: true
+  resolvers
 };
 
 const app = express();
@@ -26,7 +24,7 @@ app.use(cors());
 
 const apolloServer = new ApolloServer(apolloOptions);
 
-apolloServer.applyMiddleware({app, path: '/'});
+apolloServer.applyMiddleware({app, path: '/', cors: {origin: '*'}});
 
 app.listen(process.env.PORT || parseInt(process.env.SERVER_PORT), () => {
   mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useFindAndModify: false });
